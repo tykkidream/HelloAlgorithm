b546de5f1e32511e8c8e461a8a154ffb06b01a78
Tykkidream <tykkidream@gmail.com>
2023-04-21 12:01:44 +0800

    学习冒泡排序

--- sort-lesson/sort-lesson01/src/main/java/hello/algorithm/sort/lesson01/BubbleSort03.java
+++ sort-lesson/sort-lesson01/src/main/java/hello/algorithm/sort/lesson01/BubbleSort03.java
@@ -7,7 +7,7 @@ import com.alibaba.fastjson.serializer.SerializerFeature;
  * 冒泡排序
  * Created by Tykkidream on 2017/10/17.
  */
-public class BubbleSort02 {
+public class BubbleSort03 {
     public static void main(String[] args) {
         int[] data = new int[]{5,2,7,8,1,5,62,31,5,34};
 
@@ -21,13 +21,11 @@ public class BubbleSort02 {
     public static void bubbleSort(int[] data){
 
         for (int i = data.length - 1; i > 0; i--) {
-            for (int j = 0; j < i; j++) {
-                int a = data[j];
-                int b = data[j+1];
-
-                if (a > b) {
-                    data[j] = b;
-                    data[j+1]  = a;
+            for (int j = 0, k = 1; k < i; j = k, k++) {
+                if (data[j] > data[k]) {
+                    int temp = data[j];
+                    data[j] = data[k];
+                    data[k] = temp;
                 }
             }
         }
