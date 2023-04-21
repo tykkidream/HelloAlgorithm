5e950365616693dc8848ed6ca905e6309dbba9f6
Tykkidream <tykkidream@gmail.com>
2023-04-21 16:50:57 +0800

    学习插入排序

--- sort-lesson/sort-lesson02/src/main/java/hello/algorithm/sort/lesson02/InsertionSort04.java
+++ sort-lesson/sort-lesson02/src/main/java/hello/algorithm/sort/lesson02/InsertionSort04.java
@@ -7,7 +7,7 @@ import com.alibaba.fastjson.serializer.SerializerFeature;
  * 插入排序—直接插入排序
  * Created by Tykkidream on 2017/10/17.
  */
-public class InsertionSort03 {
+public class InsertionSort04 {
 
     public static void main(String[] args) {
         int[] data = new int[]{5,2,7,8,1,5,62,31,5,34};
@@ -22,8 +22,8 @@ public class InsertionSort03 {
     public static void insertSort(int[] data) {
         for (int i = 1; i < data.length; i++) {
             for (int j = i - 1, k = i; j > -1; k = j, j--) {
-                // 正序排序
-                if (data[j] > data[k]) {
+                // 倒序排序
+                if (data[j] < data[k]) {
                     int temp = data[j];
                     data[j] = data[k];
                     data[k] = temp;
